87b1e2162d30a498817e2dfad752bc97d3618cb2
Tony Baker <this.tonybaker@gmail.com>
2019-06-22 01:09:25 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -31,6 +31,7 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
+    ### bug fix
     if !params[:owner].keys.include?("pet_ids")
     params[:owner]["pet_ids"] = []
     end
