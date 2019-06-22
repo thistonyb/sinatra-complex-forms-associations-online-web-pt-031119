c55ce701a0f028595cc98c5678c08d26bf0b41ee
Tony Baker <this.tonybaker@gmail.com>
2019-06-22 01:25:18 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -41,6 +41,6 @@ class OwnersController < ApplicationController
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    redirect to "owners/#{@owner.id}/edit"
+    redirect to "owners/#{@owner.id}/"
   end
 end
